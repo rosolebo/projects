b883fed3ac710aaebe5cd11a1de10dac73d5ecd3
Rasaki Solebo <arosolebo@gmail.com>
2023-10-17 13:33:28 -0500

    added chapter 55

---
+++ lpn-qapp/ph-qapp/ch/ch55/question.js
@@ -0,0 +1,101 @@
+
+"use strict";
+//focus on Nursing pharmacology 
+//chapter-55 Drugs Acting on the Lower Respiratory Tract
+//Items - 10
+//console.log("hello")
+//Array of objects 
+const quiz = [
+  {//question-001
+	  q:'Treatment of obstructive pulmonary disorders is aimed at',
+	  options:['opening the conducting airways or decreasing the effects of inflammation.',
+	  'blocking the autonomic reflexes that alter respirations.',
+	  'blocking the effects of the immune and inflammatory systems.',
+	  'altering the respiratory membrane to increase the flow of oxygen and carbon dioxide.'],
+	  answer:0
+	  //Ans:  a )	  
+  },
+  {//question-002
+	q:'The xanthines',
+	  options:['block the sympathetic nervous system.',
+	  'stimulate the sympathetic nervous system.',
+	  'directly affect the smooth muscles of the respiratory tract.',
+	  'act in the CNS to cause bronchodilation.'],
+	  answer:2
+	  //Ans:  c )		  
+  },
+  {//question-003
+	  q:'Your patient has been maintained on theophylline for many years and has recently taken up smoking. The theophylline levels in this patient would be expected to',
+	  options:['rise because nicotine prevents the breakdown of theophylline.',
+	  'stay the same because smoking has no effect on theophylline.',
+	  'fall because substances in cigarettes stimulate liver metabolism of theophylline.',
+	  'rapidly reach toxic levels.'],
+	  answer:2
+	  //Ans: c )		  
+  },
+  {//question-004
+	q:'A person with hypertension and known heart disease has frequent bronchospasms and asthma attacks that are most responsive to sympathomimetic drugs. This patient might be best treated with',
+	  options:['an inhaled sympathomimetic to decrease systemic effects.',
+	  'a xanthine.',
+	  'no sympathomimetics because they would be contraindicated.',
+	  'an anticholinergic.'],
+	  answer:0
+	  //Ans: a )		  
+  },
+  {//question-005
+	q:'A patient with many adverse reactions to drugs is tried on an inhaled steroid for treatment of bronchospasm. For the first 3 days, the patient does not notice any improvement. You should',
+	  options:['switch the patient to a xanthine.',
+	  'encourage the patient to continue the drug for 2 to 3 weeks.',
+	  'switch the patient to a sympathomimetic.',
+	  'try the patient on surfactant.'],
+	  answer:1
+	  //Ans: b )	  
+  },
+  {//question-006
+    q:'Leukotriene receptor antagonists act to block production of a component of slow-reacting substance of anaphylaxis. They are most beneficial in treating',
+	  options:['seasonal rhinitis.',
+	  'pneumonia.',
+	  'COPD.',
+	  'asthma.'],
+	  answer:3
+	  //Ans: d )  
+  },
+  {//question-007
+    q:'Respiratory distress syndrome occurs in',
+	  options:['babies with frequent colds.',
+	  'babies with genetic allergies.',
+	  'premature and low-birth-weight babies.',
+	  'babies stressed during the pregnancy.'],
+	  answer:2
+	  //Ans: c )
+   },
+   {//question-008
+	 q:'Lung surfactants used therapeutically are',
+	  options:['injected into a developed muscle.',
+	  'instilled via a nasogastric tube.',
+	  'injected into the umbilical artery.',
+	  'instilled into an endotracheal tube properly placed in the baby’s lungs.'],
+	  answer:3
+	  //Ans: d )
+  },
+  {//question-009
+	  q:'Select All That Apply - Using Option A, B, C, D Below',
+	  options:['a, b, f',
+	  'b, c, e, f',
+	  'a, b, d, e',
+	  'a, c, d, f'],
+	  answer:1,
+	  img:'img/sata55-1.jpg'
+	  //Ans: b, c, e, f (Select All That Apply) 	  
+  },
+  {//question-010
+	  q:'Select All That Apply - Using Option A, B, C, D Below',
+	  options:['b, c, e, f',
+	  'b, d, e',
+	  'a, b, e, f',
+	  'a, c, d, f'],
+	  answer:3,	
+	  img:'img/sata55-2.jpg'
+	  //Ans: a, c, d, f (Select All That Apply) 
+  }	  
+]
