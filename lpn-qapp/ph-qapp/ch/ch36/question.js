829b625d3b3304d3ce1abae985d57ddbfc9c12a3
Rasaki Solebo <arosolebo@gmail.com>
2023-10-19 00:50:25 -0500

    added chapter 36

---
+++ lpn-qapp/ph-qapp/ch/ch36/question.js
@@ -0,0 +1,101 @@
+
+"use strict";
+//focus on Nursing pharmacology 
+//chapter-36 Drugs Acting on the Lower Respiratory Tract
+//Items - 10
+//console.log("hello")
+//Array of objects 
+const quiz = [
+  {//question-001
+	  q:'Adrenocortical agents are widely used',
+	  options:['to cure chronic inflammatory disorders.',
+	  'for short-term treatment to relieve inflammation.',
+	  'for long-term treatment of chronic disorders.',
+	  'to relieve minor aches and pains and to make people feel better.'],
+	  answer:1
+	  //Ans: b )	  
+  },
+  {//question-002
+	q:'If a nurse was asked to explain the adrenal medulla to a patient, it would be appropriate for him or her to tell that patient that it',
+	  options:['is the outer core of the adrenal gland.',
+	  'is the site of production of aldosterone and corticosteroids.',
+	  'is actually a neural ganglion of the SNS.',
+	  'consists of three layers of cells that produce different hormones.'],
+	  answer:2
+	  //Ans: c )		  
+  },
+  {//question-003
+	  q:'Glucocorticoids are hormones that',
+	  options:['are released in response to high glucose levels.',
+	  'help regulate electrolyte levels.',
+	  'help regulate water balance in the body.',
+	  'promote the preservation of energy through increased glucose levels, protein breakdown, and fat formation.'],
+	  answer:3
+	  //Ans: d )	  
+  },
+  {//question-004
+	q:'Diurnal rhythm in a person with a regular sleep cycle would show',
+	  options:['high levels of ACTH during the night while sleeping.',
+	  'rising levels of corticosteroids throughout the day.',
+	  'peak levels of ACTH and corticosteroids early in the morning.',
+	  'hypothalamic stimulation to release CRH around noon.'],
+	  answer:2
+	  //Ans: c )		  
+  },
+  {//question-005
+	q:'Patients who have been receiving corticosteroid therapy for a prolonged period and suddenly stop the drug will experience an adrenal crisis because their adrenal glands will not be producing any adrenal hormones. Your assessment of a patient for the possibility of adrenal crisis may include',
+	  options:['physiological exhaustion, shock, and fluid shift.',
+	  'acne development and hypertension.',
+	  'water retention and increased speed of healing.',
+	  'hyperglycemia and water retention.'],
+	  answer:0
+	  //Ans: a )	  
+  },
+  {//question-006
+    q:'A patient is started on a regimen of prednisone because of a crisis in her ulcerative colitis. Nursing care of this patient would need to include',
+	  options:['immunizations to prevent infections.',
+	  'increased calories to deal with metabolic changes.',
+	  'fluid restriction to decrease water retention.',
+	  'administration of the drug around 8 or 9 AM to mimic normal diurnal rhythm.'],
+	  answer:3
+	  //Ans: d )  
+  },
+  {//question-007
+    q:'A patient who is taking corticosteroids is at increased risk for infection and should',
+	  options:['be protected from exposure to infections and invasive procedures.',
+	  'take antiinflammatory agents regularly throughout the day.',
+	  'receive live virus vaccine to protect him or her from infection.',
+	  'be at no risk if elective surgery is needed.'],
+	  answer:0
+	  //Ans: a )
+   },
+   {//question-008
+	 q:'Mineralocorticoids are used to maintain electrolyte balance in situations of adrenal insufficiency. Mineralocorticoids',
+	  options:['are usually given alone.',
+	  'can be given only IV.',
+	  'are always given in conjunction with appropriate glucocorticoids.',
+	  'are separate in their function from the glucocorticoids.'],
+	  answer:2
+	  //Ans: c )
+  },
+  {//question-009
+	  q:'Select All That Apply - Using Option A, B, C, D Below',
+	  options:['a, b, f',
+	  'b, c, e, f',
+	  'a, b, c, f',
+	  'a, c, d, f'],
+	  answer:2,
+	  img:'img/sata36-1.jpg'
+	  //Ans: a, b, c, f(Select All That Apply) 	  
+  },
+  {//question-010
+	  q:'Select All That Apply - Using Option A, B, C, D Below',
+	  options:['b, c, e, f',
+	  'b, d, e',
+	  'a, b, e, f',
+	  'a, b, d, f'],
+	  answer:3,	
+	  img:'img/sata36-2.jpg'
+	  //Ans:a, b, d, f(Select All That Apply) 
+  }	  
+]
