0f80851767ac7a1c958185f1439f6c40ad8585e9
Rasaki Solebo <arosolebo@gmail.com>
2023-10-23 14:13:58 -0500

    added chapter 50

---
+++ lpn-qapp/ph-qapp/ch/ch50/question.js
@@ -0,0 +1,102 @@
+
+"use strict";
+//focus on Nursing pharmacology 
+//chapter-50 Introduction to the Renal System
+//Items - 10
+//console.log("hello")
+//Array of objects 
+const quiz = [
+  {//question-001
+	  q:'During severe exertion, a man may lose up to 4 L of hypotonic sweat per hour. This loss would result in',
+	  options:['decreased plasma volume.',
+	  'decreased plasma osmolarity.',
+	  'decreased circulating levels of ADH.',
+	  'return of body fluid balance to normal after ingestion of 100 mL of water.'],
+	  answer:0
+	  //Ans: a )	  
+  },
+  {//question-002
+	  q:'Urine passes through the ureter by',
+	  options:['osmosis.',
+	  'air pressure.',
+	  'filtration.',
+	  'peristalsis.'],
+	  answer:3
+	  //Ans:  d )				  
+  },
+  {//question-003
+	  q:'When describing renal reabsorption to a group of students, the instructor would identify it as the movement of which?',
+	  options:['Substances from the renal tubule into the blood',
+	  'Substances from the blood into the renal tubule',
+	  'Water that is increased in the absence of ADH',
+	  'Sodium occurring only in the proximal tubule'],
+	  answer:0
+	  //Ans: a )				  
+  },
+  {//question-004
+	  q:'Considering the functions of the kidney, if a patient lost kidney function, a nurse would expect to see',
+	  options:['increased red blood cell count.',
+	  'decreased fluid volume.',
+	  'low blood potassium.',
+	  'variability in control of blood pressure.'],
+	  answer:3
+	  //Ans: d )				  
+  },
+  {//question-005
+	  q:'Blood flow to the nephron differs from blood flow to other tissues in that',
+	  options:['the venous system is not involved in blood flow around the nephron.',
+	  'there are no capillaries in the nephron allowing direct flow from the artery to the vein.',
+	  'efferent and afferent arterioles allow for autoregulation of blood flow.',
+	  'the capillary bed has a fenestrated membrane to allow passage of fluid and small particles.'],
+	  answer:2
+	  //Ans: c )			  
+  },
+  {//question-006
+	  q:'Concentration and dilution of urine are controlled by',
+	  options:['afferent arterioles.',
+	  'the renin–angiotensin–aldosterone system.',
+	  'aldosterone release.',
+	  'the countercurrent mechanism.'],
+	  answer:3
+	  //Ans: d )		 
+  },
+  {//question-007
+	  q:'Women tend to have more problems with bladder infections than men because',
+	  options:['women have Escherichia coli in the urinary tract.',
+	  'women have a short urethra, making access to the bladder easier for bacteria.',
+	  'the prostate gland secretes a substance that protects men from bladder infections.',
+	  'women’s urine is more acidic, encouraging the growth of bladder bacteria.'],
+	  answer:1
+	  //Ans: b )		
+   },
+   {//question-008
+	  q:'Select All That Apply - Using Option A, B, C, D Below',
+	  options:['a, b, f',
+	  'a, b, c, e, f',
+	  'a, b, d, e',
+	  'a, f'],
+	  answer:1,
+	  img:'img/sata50-1.jpg'
+	  //Ans: a, b, c, e, f  (Select All That Apply) 	  
+  },
+  {//question-009
+	  q:'Select All That Apply - Using Option A, B, C, D Below',
+	  options:['a, b, f',
+	  'b, c, e',
+	  'a, b, d, e',
+	  'a, c, d, e, f'],
+	  answer:3,
+	  img:'img/sata50-2.jpg'
+	  //Ans: a, c, d, e, f  (Select All That Apply) 	  
+  },
+  {//question-010
+	  q:'Select All That Apply - Using Option A, B, C, D Below',
+	  options:['a, c, e, f',
+	  'a, b, e, f',
+	  'a, c, d, f',
+	  'b, c, d, f'],
+	  answer:2,	
+	  img:'img/sata50-3.jpg'
+	  //Ans: a, c, d, f(Select All That Apply) 
+  }	  
+]
