0c7e2b1f45c39629642e371fabc640a3cc193e08
Rasaki Solebo <arosolebo@gmail.com>
2023-10-25 09:22:40 -0500

    edited media screen properties

--- lpn-qapp/ph-qapp/ch/cf50/datas.js
+++ lpn-qapp/ph-qapp/ch/cf50/datas.js
@@ -5,22 +5,26 @@ console.log("Flashcard App Connected");
 /* Change the questions to whatever you want! Remember to stick to the theme of music this month. */
 var questions=[
     "Aldosterone",
-    "filtration",
-    "glomerulus",
-    "nephron",
-    "reabsorption",
-    "secretion",
-    "prostate gland",
-    "antidiuretic hormone (ADH)"
+    "Filtration",
+    "Glomerulus",
+    "Nephron",
+    "Reabsorption",
+    "Secretion",
+    "Prostate gland",
+    "Antidiuretic hormone (ADH)",
+    "Detrusor muscle",
+    "Urethra"
 ];
 
 var answers=[
     "hormone produced by the adrenal gland that causes the distal tubule to retain sodium, and therefore water, while losing potassium into the urine",
     "passage of fluid and small components of the blood through the glomerulus into the nephron tubule",
-    "the tuft of blood vessel between the afferent and efferent arterioles in the nephron; the fenestrated membrane of the glomerulus allows filtration of fluid from the blood into the nephron tubule",
+    "the tuft of blood vessel between the afferent and efferent arterioles in the nephron;",
     "functional unit of the kidney, composed of Bowman capsule, the proximal and distal convoluted tubules, and the collecting duct",
     "the movement of substances from the renal tubule back into the vascular system",
     "the active movement of substances from the blood into the renal tubule for excretion",
     "gland located around the male urethra; responsible for producing an acidic fluid that maintains sperm and lubricates the urinary tract",
-    "hormone produced by the hypothalamus and stored in the posterior pituitary gland"
+    "hormone produced by the hypothalamus and stored in the posterior pituitary gland",
+    "The detrusor muscle is the muscularis propria component of the bladder, which primary function is to contract during voiding events to expel urine in the bladder, into urethra.",
+    "In the female, the urethra is a short tube that leads from the bladder to an area populated by normal flora, including Escherichia coli, which can cause frequent bladder infections or cystitis."
 ];
