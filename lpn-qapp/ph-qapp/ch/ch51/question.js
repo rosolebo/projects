57c9c65b129e3546536ead02487e3151f1515ee1
Rasaki Solebo <arosolebo@gmail.com>
2023-10-25 15:07:55 -0500

    added chapter 51

---
+++ lpn-qapp/ph-qapp/ch/ch51/question.js
@@ -0,0 +1,102 @@
+
+"use strict";
+//focus on Nursing pharmacology 
+//chapter-51 Diuretic Agents
+//Items - 10
+//console.log("hello")
+//Array of objects 
+const quiz = [
+  {//question-001
+	  q:'Most diuretics act in the body to cause',
+	  options:['loss of calcium.',
+	  'loss of sodium.',
+	  'retention of potassium.',
+	  'retention of chloride.'],
+	  answer:1
+	  //Ans: b )	  
+  },
+  {//question-002
+	  q:'Diuretics cause a loss of fluid volume in the body. The drop in volume activates compensatory mechanisms to restore the volume, including',
+	  options:['suppression of ADH release and stimulation of the countercurrent mechanism.',
+	  'suppression of aldosterone release and increased ADH release.',
+	  'activation of the renin–angiotensin–aldosterone system with increased ADH and aldosterone.',
+	  'stimulation of the countercurrent mechanism with reflex drop in renin release.'],
+	  answer:2
+	  //Ans: c )				  
+  },
+  {//question-003
+	  q:'Thiazide diuretics are considered mild diuretics because',
+	  options:['they block the sodium pump in the loop of Henle.',
+	  'they cause loss of sodium and chloride but little water.',
+	  'they do not cause fluid rebound when they work in the kidneys.',
+	  'they have little or no effect on electrolyte levels.'],
+	  answer:1
+	  //Ans: b )				  
+  },
+  {//question-004
+	  q:'The nurse would anticipate an order for a loop diuretic as the drug of choice for a patient with',
+	  options:['hypertension.',
+	  'septic shock.',
+	  'pulmonary edema.',
+	  'fluid retention of pregnancy.'],
+	  answer:2
+	  //Ans: c )				  
+  },
+  {//question-005
+	  q:'When providing care to a patient who is receiving a loop diuretic, the nurse would determine the need to regularly monitor which?',
+	  options:['Sodium levels',
+	  'Bone marrow function',
+	  'Calcium levels',
+	  'Potassium levels'],
+	  answer:3
+	  //Ans: d )			  
+  },
+  {//question-006
+	  q:'When developing the plan of care for a patient with hyperaldosteronism, the nurse would expect the physician to prescribe which agent?',
+	  options:['Spironolactone',
+	  'Furosemide',
+	  'Hydrochlorothiazide',
+	  'Acetazolamide'],
+	  answer:0
+	  //Ans: a )		 
+  },
+  {//question-007
+	  q:'A patient with severe glaucoma who is about to undergo eye surgery would benefit from a decrease in intraocular fluid. This is often best accomplished by giving the patient',
+	  options:['a loop diuretic.',
+	  'a thiazide diuretic.',
+	  'a carbonic anhydrase inhibitor.',
+	  'an osmotic diuretic.'],
+	  answer:3
+	  //Ans: d )	
+   },
+   {//question-008
+	  q:'The nurse would instruct a patient receiving a loop diuretic to report',
+	  options:['yellow vision.',
+	  'weight loss of 1 lb/d.',
+	  'muscle cramping.',
+	  'increased urination.'],
+	  answer:2
+	  //Ans: c )	
+   },
+   {//question-009
+	  q:'Select All That Apply - Using Option A, B, C, D Below',
+	  options:['a, b, c, f',
+	  'a, b, d, f',
+	  'a, b, d, e',
+	  'a, f'],
+	  answer:1,
+	  img:'img/sata51-1.jpg'
+	  //Ans: a, b, d, f  (Select All That Apply) 	  
+  },
+  
+  {//question-010
+	  q:'Select All That Apply - Using Option A, B, C, D Below',
+	  options:['a, c, e, f',
+	  'a, b, e, f',
+	  'a, c, d, f',
+	  'b, c, d, f'],
+	  answer:2,	
+	  img:'img/sata51-2.jpg'
+	  //Ans: a, c, d, f(Select All That Apply) 
+  }	  
+]
