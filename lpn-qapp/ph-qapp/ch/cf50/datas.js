d0a9bedfae8b123a988b9b220c19199b2a80ef91
Rasaki Solebo <arosolebo@gmail.com>
2023-10-24 23:59:49 -0500

    added ch50 flashcard

---
+++ lpn-qapp/ph-qapp/ch/cf50/datas.js
@@ -0,0 +1,26 @@
+"use strict";
+//connect
+console.log("Flashcard App Connected");
+
+/* Change the questions to whatever you want! Remember to stick to the theme of music this month. */
+var questions=[
+    "Aldosterone",
+    "filtration",
+    "glomerulus",
+    "nephron",
+    "reabsorption",
+    "secretion",
+    "prostate gland",
+    "antidiuretic hormone (ADH)"
+];
+
+var answers=[
+    "hormone produced by the adrenal gland that causes the distal tubule to retain sodium, and therefore water, while losing potassium into the urine",
+    "passage of fluid and small components of the blood through the glomerulus into the nephron tubule",
+    "the tuft of blood vessel between the afferent and efferent arterioles in the nephron; the fenestrated membrane of the glomerulus allows filtration of fluid from the blood into the nephron tubule",
+    "functional unit of the kidney, composed of Bowman capsule, the proximal and distal convoluted tubules, and the collecting duct",
+    "the movement of substances from the renal tubule back into the vascular system",
+    "the active movement of substances from the blood into the renal tubule for excretion",
+    "gland located around the male urethra; responsible for producing an acidic fluid that maintains sperm and lubricates the urinary tract",
+    "hormone produced by the hypothalamus and stored in the posterior pituitary gland"
+];
