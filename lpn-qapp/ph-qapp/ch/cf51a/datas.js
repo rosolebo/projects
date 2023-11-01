c5ae8138b118e81d7add074869322773dcdbb942
Rasaki Solebo <arosolebo@gmail.com>
2023-11-01 08:30:33 -0500

    added chapter50-51 flash cards

---
+++ lpn-qapp/ph-qapp/ch/cf51a/datas.js
@@ -0,0 +1,54 @@
+"use strict";
+//connect
+console.log("Flashcard App Connected");
+
+/* Change the questions to whatever you want! Remember to stick to the theme of music this month. */
+var questions=[
+    "What is the medical terminology for Bladder?",
+    "What is the medical terminilogy for Gall or Bile?",
+    "What is the medical terminology for gall ladder?",
+    "What are the indications for diuretic medication use?",
+    "What is the function of diuretic agents?",
+    "What is the site of action for Osmotic diuretic?",
+    "What is the site of action for Carbonic anhydrase inhibitors diuretics?",
+    "What is the site of action for Loop diuretics?",
+    "What is the site of action for Thiazide diuretics?",
+    "What is the site of action for Potassium sparing diuretics?",
+    "What are the Osmotic diuretics medications?",
+    "What diuretic medication pulls water into the renal tubule without sodium loss?",
+    "What are the Carbonic anhydrase inhibitor diuretics medications?",
+    "What are the Loop diuretics medications?",
+    "What are the Thiazide diuretics medications?",
+    "What are the Thiazide-like diuretics medications?",
+    "What are the Potassium sparing diuretics medications?",
+    "What diuretic medication causes Acidosis?",
+    "Potassium sparing diuretics agents are divided into two?",
+    "The two Aterioles connected to the Glomerulus are:",
+    "Adverse effect of Thiazide diuretics are",
+    "HyperGLUC full details are"
+];
+
+var answers=[
+    "Cyst",
+    "Chole",
+    "Chole cyst",
+    "1-Edema assoc. with Congestive heart failure (CHF), 2-Acute pulmonary Edema, 3-Liver Disease, 4-Renal disease, 5-Hypertension, 6-Condition that cause hyperkalemia",
+    "1-Increase the amount of urine produced by the kidney, 2-Increase sodium excretion.",
+    "Glomerulus, Bowman's capsule",
+    "Proximal Convoluted Tubule",
+    "Loop of Henle",
+    "Distal Convoluted Tubule",
+    "Collecting duct",
+    "Mannitol (Osmitol)",
+    "Mannitol",
+    "1-Acetazolamide, 2-Dichlorphenamide, 3-Methazolamide",
+    "1-Ethacrynic, 2-Bumetanide, 3-Furosemide, 4-Torsemide",
+    "1-Hydrochlorothiazide, 2-Chlorothiazide, 3-Methyclothiazide",
+    "1-Chlorthalidone, 2-Indapamide, 3-Metolazone",
+    "1-Spironolactone, 2-Eplerenone, 3-Amiloride, 4-Triamterene",
+    "Acetazolamide",
+    "1-Aldosterone antagonists, 2-Sodium Channel Blockers",
+    "1-Afferent Arteriole, 2-Efferent Arteriole",
+    "DHHA: 1-Dehydration, 2-Hypotension, 3-Hypokalemia, 4-Alkaldosis, 5-HylerGLUC",
+    "1-Glucose, 2-Lipids, 3-Uric acid, 4-Calcium"
+];
