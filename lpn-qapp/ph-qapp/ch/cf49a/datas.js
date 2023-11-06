6cc049396895ad7c8fa10db5a07897738527acc0
Rasaki Solebo <arosolebo@gmail.com>
2023-11-06 01:29:04 -0600

    added poison and antidote

---
+++ lpn-qapp/ph-qapp/ch/cf49a/datas.js
@@ -0,0 +1,55 @@
+"use strict";
+//connect
+console.log("Flashcard App Connected");
+//chapter-Antidotes - Poisons
+
+/* Change the questions to whatever you want! Remember to stick to the theme of music this month. */
+var questions=[
+    "What is the antidote for Coumadin (Wafarin)?",
+    "What is the antidote for benzodiazepines?",
+    "What is the antidote for magnesium sulfate?",
+    "What is the antidote for heparin?",
+    "What is the antidote for tylenol?",
+    "What is the antidote for opiates?",
+    "What is the antidote for cholinergic drugs?",
+    "What is the antidote for digoxin?",
+    "What is the antidote for acetominophen?",
+    "What is the antidote for ferrous sulfate?",
+    "What is the antidote for alcohol withdrawal?",
+    "What is the antidote for beta blockers?",
+    "What is the antidote for methotrexate?",
+    "What is the antidote for aspirin?",
+    "What is the antidote for CCB?",
+    "What is the antidote for anticoagulants?",
+    "What's the Antidote for High potassium (K+)?",
+    "What's the Antidote for Anti-Cholinergics",
+    "What's the Antidote for Asprin?",
+    "What's the Antidote for Methanol?",
+    "What's the Antidote for methemoglobin?",
+    "What's the Antidote for Tricyclic antidepressants?"
+];
+
+var answers=[
+    "Vitamin K",
+    "Romazicon (Flumazenil)",
+    "Calcium gluconate or Calcium chloride",
+    "protamine sulfate",
+    "Mucomyst",
+    "Naloxone (Narcan), narcotic analgesics, or heroin morphine",
+    "Atropine or pralidoxime (2-PAM)",
+    "Digibind",
+    "N-acetylcysteine",
+    "Deferoxamine",
+    "Librium",
+    "glucagon",
+    "Leucovorin",
+    "sodium bicarbonate",
+    "Calcium, glucagon, or insulin",
+    "Vitamin K",
+    "Insulin IV, Sodium Polystyrene sulfonate",
+    "Physostigmine",
+    "Sodium Bicarbonate",
+    "Ethanol",
+    "Methylene blue",
+    "Sodium bicarbonate"
+];
