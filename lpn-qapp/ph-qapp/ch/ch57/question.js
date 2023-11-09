2acd4a8d60233b615775f66e1d9211a54e4c7441
Rasaki Solebo <arosolebo@gmail.com>
2023-11-09 13:16:00 -0600

    added chap 56-59

---
+++ lpn-qapp/ph-qapp/ch/ch57/question.js
@@ -0,0 +1,101 @@
+
+"use strict";
+//focus on Nursing pharmacology 
+//chapter-57 Drugs Affecting Gastrointestinal Secretions
+//Items - 10
+//console.log("hello")
+//Array of objects 
+const quiz = [
+  {//question-001
+	  q:'Which would a nurse include when describing the action of H2 antagonists to a patient?',
+	  options:['They block the release of gastrin and pepsin, leading to a decrease in protein digestion.',
+	  'They selectively block histamine receptors, reducing swelling and inflammation at numerous sites.',
+	  'They selectively block specific histamine receptor sites, leading to a reduction in gastric acid secretion.',
+	  'They are effective primarily for long-term use because of their slow onset of action.'],
+	  answer:2	
+	  // answer- c 
+  },
+  {//question-002
+	  q:'H2 receptors are found throughout the body, including',
+	  options:['in the nasal passages, upper airways, and stomach.',
+	  'in the CNS and upper airways.',
+	  'in the respiratory tract and the heart.',
+	  'in the heart, CNS, and stomach.'],
+	  answer:3	
+	  // answer- d	  
+  },
+  {//question-003
+	  q:'Which H2 antagonist would the nurse expect to be ordered for a patient with known liver dysfunction?',
+	  options:['Cimetidine',
+	  'Famotidine',
+	  'Nizatidine',
+	  'Ranitidine'],
+	  answer:2	
+	  // answer- c	  
+  },
+  {//question-004
+	  q:'Which condition would the nurse need to monitor more closely if a patient was receiving IV cimetidine (generic) versus the oral form of the medication?',
+	  options:['GI upset',
+	  'Gynecomastia',
+	  'Cardiac arrhythmias',
+	  'Constipation'],
+	  answer:2	
+	  // answer- c	  
+  },
+  {//question-005
+	  q:'Acid rebound is a condition that occurs when',
+	  options:['lowering gastric acid to an alkaline level stimulates the release of gastric acid.',
+	  'raising gastric acid levels causes heartburn.',
+	  'combining protein, calcium, and smoking greatly elevates gastric acid levels.',
+	  'eating citrus fruit neutralizes gastric acid.'],
+	  answer:0
+	  // answer- a  
+  },
+  {//question-006
+	  q:'A nurse taking care of a patient who is receiving a proton pump inhibitor should teach the patient to',
+	  options:['take the drug after every meal.',
+	  'chew or crush tablets to increase their absorption.',
+	  'swallow tablets or capsules whole.',
+	  'stop taking the drug after 3 weeks of therapy.'],
+	  answer:2
+	  // answer- c	  
+  },
+  {//question-007
+	  q:'Misoprostol (Cytotec) is a prostaglandin that is used to',
+	  options:['prevent uterine contractions.',
+	  'prevent NSAID-related gastric ulcers in patients at high risk.',
+	  'decrease hyperacidity with meals and at bedtime.',
+	  'relieve the burning associated with hiatal hernia at night.'],
+	  answer:1	
+	  // answer- b
+  },
+  {//question-008
+	  q:'A nurse caring for a patient receiving pancreatic enzymes as replacement therapy should be assessing the patient for',
+	  options:['hypertension.',
+	  'cardiac arrhythmias.',
+	  'excessive weight gain.',
+	  'signs of GI irritation.'],
+	  answer:3
+	  // answer- d		  
+  },
+  {//question-009
+	  q:'Select All That Apply - Using Option A B C D Below',
+	  options:['a, c, d, f',
+	  'a, b, c, e',
+	  'a, c, d, e',
+	  'a, b, c, e, f'],
+	  answer:3,
+	  img:'img/sata57-1.jpg'
+	  //Ans: a, b, c, e, f (Select All That Apply) 	  
+  },
+  {//question-010
+	  q:'Select All That Apply - Using Option A B C D Below',
+	  options:['a, b, c, f',
+	  'a, b, d',
+	  'a, c, e, f',
+	  'c, d, f'],
+	  answer:1,	
+	  img:'img/sata57-2.jpg'
+	  //Ans: a, b, d (Select All That Apply) 
+  }	  
+]
---
+++ lpn-qapp/ph-qapp/ch/ch59/question.js
@@ -0,0 +1,74 @@
+
+"use strict";
+//focus on Nursing pharmacology 
+//chapter-59 Antiemetic Agents
+//Items - 10
+//console.log("hello")
+//Array of objects 
+const quiz = [
+  {//question-001
+	  q:'The nurse anticipates prochlorperazine (Compazine) would be the antiemetic of choice for which condition?',
+	  options:['Nausea and vomiting after anesthesia',
+	  'Nausea and vomiting due to cancer chemotherapy',
+	  'Motion sickness',
+	  'Intractable hiccoughs'],
+	  answer:0	
+	  // answer- a 
+  },
+  {//question-002
+	  q:'Most antiemetics work with the CNS to decrease the activity of',
+	  options:['the medulla.',
+	  'the chemoreceptor trigger zone.',
+	  'the respiratory center.',
+	  'the sympathetic nervous system.'],
+	  answer:1	
+	  // answer- b	  
+  },
+  {//question-003
+	  q:'Which instruction would be most appropriate to give to a patient to reduce the risk of photosensitivity related to the use of antiemetic agents?',
+	  options:['Avoid having your picture taken.',
+	  'Cover the head at extremes of temperature.',
+	  'Take extra precautions to avoid heat stroke.',
+	  'Wear protective clothing when in the sun.'],
+	  answer:3	
+	  // answer- d	  
+  },
+  {//question-004
+	  q:'The 5-HT3 receptor blockers, including ondansetron (Zofran) and granisetron (Kytril), are particularly effective in decreasing the nausea and vomiting associated with',
+	  options:['vestibular problems.',
+	  'cancer chemotherapy.',
+	  'pregnancy.',
+	  'severe pain.'],
+	  answer:1	
+	  // answer- b	  
+  },
+  {//question-005
+	  q:'A parent calls with concerns that a 2-year-old child ate a bottle full of baby aspirin. The nurse would advise the parent to',
+	  options:['administer ipecac immediately.',
+	  'induce vomiting by inserting a finger against the back of the child’s throat.',
+	  'force fluids as the parent brings the child in for evaluation.',
+	  'feed the child charcoal.'],
+	  answer:2
+	  // answer- c  
+  },
+  {//question-006
+	  q:'Select All That Apply - Using Option A B C D Below',
+	  options:['a, b, d, f',
+	  'a, b, c, e, f',
+	  'a, c, d, e, f',
+	  'a, b, c, f'],
+	  answer:2,
+	  img:'img/sata59-1.jpg'
+	  //Ans:a, c, d, e, f (Select All That Apply) 	  
+  },
+  {//question-007
+	  q:'Select All That Apply - Using Option A B C D Below',
+	  options:['b, c',
+	  'a, c',
+	  'a, b, c',
+	  'b, e, f'],
+	  answer:1,	
+	  img:'img/sata59-2.jpg'
+	  //Ans: a, c (Select All That Apply) 
+  }	  
+]
